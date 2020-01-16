9e6280429cf4844ebf2c20febad9386f7184a9fc
ayoubhamaoui <ayoub10hamaoui@gmail.com>
2020-01-17 00:03:59 +0100

    first commit

---
+++ src/main/resources/import_data.sql
@@ -0,0 +1,4 @@
+INSERT INTO public.user values (-1, 'Belal', 0);
+INSERT INTO public.user values (-2, 'Taoufik', 0);
+INSERT INTO public.user values (-3, 'Ayoub', 0);
+INSERT INTO public.user values (-4, 'Ali', 0);
